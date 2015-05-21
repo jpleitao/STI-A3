0299de2f57855cf89698c30ee3d938c8d88c9215
jpleitao <jocaleitao93@gmail.com>
2015-05-21 16:50:00 +0100

    Can generate and load certificate, detecting errors when file corrupted

--- ChatService/src/main/java/test/Test2Client.java
+++ ChatService/src/main/java/test/Test2Client.java
@@ -3,18 +3,19 @@ package test;
 
 import org.bouncycastle.openssl.PEMReader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.security.Certificate;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 public class Test2Client {
 
     public static void main(String[] args) {
         String certificateFile = "/home/joaquim/Desktop/certificateClient.cer";
+        /*
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
         try{
@@ -25,10 +26,37 @@ public class Test2Client {
             System.out.println(cert);
             PublicKey pk = cert.getPublicKey();
             System.out.println(pk);
-
-
-
         } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            System.out.println();
+            System.out.println("CertificateFactory Object Info: ");
+            System.out.println("Type = " + cf.getType());
+            System.out.println("Provider = " + cf.getProvider());
+            System.out.println("toString = " + cf.toString());
+
+            FileInputStream fis = new FileInputStream(certificateFile);
+            java.security.cert.Certificate cert = cf.generateCertificate(fis);
+            fis.close();
+            System.out.println();
+            System.out.println("Certificate Object Info: ");
+            System.out.println("Type = " + cert.getType());
+            System.out.println("toString = " + cert.toString());
+
+            PublicKey pubKey = cert.getPublicKey();
+            System.out.println();
+            System.out.println("PublicKey Object Info: ");
+            System.out.println("Algorithm = " + pubKey.getAlgorithm());
+            System.out.println("Format = " + pubKey.getFormat());
+            System.out.println("toString = " + pubKey.toString());
+        } catch(CertificateException c){
+            c.printStackTrace();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
