b88a0efe604596062e7b4651dd4d3075c3cc3b30
jpleitao <jocaleitao93@gmail.com>
2015-05-21 23:50:01 +0100

    Saving certificates as object files

--- ChatService/src/main/java/client/CAClient.java
+++ ChatService/src/main/java/client/CAClient.java
@@ -1,9 +1,7 @@
 package client;
 
-import javax.xml.bind.DatatypeConverter;
 import java.io.*;
 import java.net.Socket;
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 
 public class CAClient {
@@ -25,9 +23,12 @@ public class CAClient {
             ObjectInputStream objectInputStream = new ObjectInputStream(cAsocket.getInputStream());
             DataOutputStream dataOutputStreamCA = new DataOutputStream(cAsocket.getOutputStream());
 
+            //Send client name
             dataOutputStreamCA.writeUTF(clientName);
+
             //Get certificate
             X509Certificate certificate = (X509Certificate) objectInputStream.readObject();
+
             //Save it to a file
             saveCertificateToFile(certificate, clientName);
         } catch (IOException | ClassNotFoundException e) {
@@ -36,34 +37,17 @@ public class CAClient {
     }
 
     private void saveCertificateToFile(X509Certificate certificate, String clientName) throws IOException {
-        FileWriter fw = new FileWriter(clientName + "-Certificate.cer");
-        fw.write(certToString(certificate));
-        fw.close();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(clientName + "-Certificate.cer"));
+        oos.writeObject(certificate);
+        oos.close();
     }
-
-    private String certToString(X509Certificate cert) {
-        StringWriter sw = new StringWriter();
-        try {
-            sw.write("-----BEGIN CERTIFICATE-----\n");
-            sw.write(DatatypeConverter.printBase64Binary(cert.getEncoded()).replaceAll("(.{64})", "$1\n"));
-            sw.write("\n-----END CERTIFICATE-----\n");
-        } catch (CertificateEncodingException e) {
-            e.printStackTrace();
-        }
-        return sw.toString();
-    }
-
 
     public static void main(String[] args) {
-
         if (args.length != 1) {
             System.out.println("Invalid arguments! Please call: java CAClient <Client-Name>");
             return ;
         }
-
         CAClient client = new CAClient();
-
         client.connectToCA(args[0]);
-
     }
 }
